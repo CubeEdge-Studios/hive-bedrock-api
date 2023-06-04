9b17a2cad7e269afd14708bf7c33a40267e83ae3
David Fiddes <davidi.fiddes@gmail.com>
2023-06-04 14:37:05 +0100

    Finish Typing

--- src/index.ts
+++
@@ -1,5 +0,0 @@
-import getStats from "./getStats";
-import fetchData from "./misc/fetchData";
-import getGame, { getAllGames } from "./misc/getGame";
-
-export { getStats, fetchData, getGame, getAllGames };
