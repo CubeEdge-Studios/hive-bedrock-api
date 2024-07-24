0f31ec0a7997d176a646b324b5bae02aa9d3b87f
UCDFiddes <66165184+UCDFiddes@users.noreply.github.com>
2024-07-24 13:48:32 +0100

    change infinity to 0

--- src/helpers/toPoint.ts
+++ src/helpers/toPoint.ts
@@ -1,4 +1,5 @@
 export default function toPoint(num: number, point: number): number {
     if (isNaN(num)) return 0;
+    if (num === Infinity) return 0;
     return Math.round(num * (10 ^ point)) / (10 ^ point);
 }
